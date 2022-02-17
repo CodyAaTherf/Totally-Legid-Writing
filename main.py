ca1d16d3ba951c13957705391ae0a66979db811b
CodyAaTherf <aatherfgamerplayzs@gmail.com>
2022-02-17 12:54:17 +0530

    Improved code as promised

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+# import pywhatkit
+# pywhatkit.text_to_handwriting("This is my handwriting xD", save_to = "writing.png")
+
 import pywhatkit
-pywhatkit.text_to_handwriting("This is my handwriting xD", save_to = "writing.png")
+pywhatkit.text_to_handwriting(input("Enter something: ") , save_to = "writing.png")
